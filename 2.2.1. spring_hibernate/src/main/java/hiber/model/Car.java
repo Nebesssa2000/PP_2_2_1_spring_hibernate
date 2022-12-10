c63a3f2558c3cb2e50eda8bdc1b935f5cce4c27d
Gabbasov Victor <victtss@gmail.com>
2022-12-11 01:21:49 +0300

    C8"+toString Car"

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/Car.java
@@ -34,7 +34,7 @@ public class Car {
     @Override
     public String toString() {
         return "Car{" +
-                ", model='" + model + '\'' +
+                "model='" + model + '\'' +
                 ", series=" + series +
                 '}';
     }
